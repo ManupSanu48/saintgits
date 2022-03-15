06b76cf66c93adba87916f1b296561c8ac46efc3
ManupSanu48 <101617007+ManupSanu48@users.noreply.github.com>
2022-03-15 10:28:24 +0530

    Add files via upload

---
+++ greatest of 2.py
@@ -0,0 +1,7 @@
+print("largest of two numbers")
+x=input("enter the first integer")
+y=input("enter the second integer")
+if x>y:
+    print(x," is greater than ",y)
+else:
+    print(y," is greater than ",x)
